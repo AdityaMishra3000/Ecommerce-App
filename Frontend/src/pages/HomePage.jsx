ea3203db8f4e26a191f99d06dee7f7ca35d03d38
AdityaMishra3000 <mr.ba8man@gmail.com>
2025-10-24 18:40:00 +0530

    lift cart state to App.jsx with the motive of sharing its values

--- Frontend/src/pages/HomePage.jsx
+++ Frontend/src/pages/HomePage.jsx
@@ -4,17 +4,12 @@ import { Header } from "../components/Header";
 import "./HomePage.css";
 // import products from "../../starting-code/backend/products.json";
 
-export function HomePage() {
+export function HomePage({ cart }) {
   const [products, setProducts] = useState([]);
-  const [cart, setCart] = useState([]);
 
   useEffect(() => {
     axios.get("/api/products").then((response) => {
       setProducts(response.data);
-    });
-
-    axios.get("/api/cart-items").then((response) => {
-      setCart(response.data);
     });
   }, []);
   return (
